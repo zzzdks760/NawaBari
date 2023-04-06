dbae96c45001ed88d1cd6da57c2ef67a2b30d7ee
zzzdks760 <zzzdks760@naver.com>
2023-04-06 16:25:05 +0900

    MemberZoneService 수정

--- src/main/java/com/backend/NawaBari/service/MemberZoneService.java
+++ src/main/java/com/backend/NawaBari/service/MemberZoneService.java
@@ -3,6 +3,7 @@ package com.backend.NawaBari.service;
 import com.backend.NawaBari.domain.Member;
 import com.backend.NawaBari.domain.MemberZone;
 import com.backend.NawaBari.domain.Zone;
+import com.backend.NawaBari.dto.MemberDTO;
 import com.backend.NawaBari.dto.ZoneDTO;
 import com.backend.NawaBari.exception.MaximumZoneLimitException;
 import com.backend.NawaBari.exception.ZoneAlreadySetException;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -20,19 +22,23 @@ public class MemberZoneService {
 
     private final MemberZoneRepository memberZoneRepository;
     private final ZoneRepository zoneRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 영역 목록 추가
      */
-    public void setMemberZone(Member member, ZoneDTO zoneDTO) {
+    public void setMemberZone(MemberDTO memberDTO, ZoneDTO zoneDTO) {
+
+        Member member = memberRepository.findOne(memberDTO.getId());
+
         if (member.getMemberZones().size() >= 2) {
             throw new MaximumZoneLimitException("이미 두 개의 구역이 설정되어 있습니다");
         }
 
-        Zone zone = zoneRepository.findOne(zoneDTO.getCig_cd());
+        Zone zone = zoneRepository.findOne(zoneDTO.getId());
 
         boolean zoneAlreadySet = member.getMemberZones().stream()
-                .anyMatch(memberZone -> memberZone.getZone().getCig_cd().equals(zone.getCig_cd()));
+                .anyMatch(memberZone -> memberZone.getZone().getId().equals(zone.getId()));
 
         if (zoneAlreadySet) {
             throw new ZoneAlreadySetException("이미 설정 된 구역입니다");
