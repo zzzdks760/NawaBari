353cb69a0e9a7cbf3419c82ea0acbd5ea81e361f
zzzdks760 <zzzdks760@naver.com>
2023-04-11 16:50:03 +0900

    @setter 열어둠

--- src/main/java/com/backend/NawaBari/domain/Member.java
+++ src/main/java/com/backend/NawaBari/domain/Member.java
@@ -1,18 +1,16 @@
 package com.backend.NawaBari.domain;
 
 import com.backend.NawaBari.domain.review.Review;
+import com.backend.NawaBari.dto.MemberDTO;
 import jakarta.persistence.*;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter
+@Getter@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends Base{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,15 +35,20 @@ public class Member extends Base{
 
 
     //생성자 주입
+
     @Builder
-    public Member(String kakao_id, String profile_nickname, String profile_image, String gender, String age, List<MemberZone> memberZones, List<Review> reviews) {
+    public Member(String kakao_id, String profile_nickname) {
         this.kakao_id = kakao_id;
         this.profile_nickname = profile_nickname;
-        this.profile_image = profile_image;
-        this.gender = gender;
-        this.age = age;
-        this.memberZones = memberZones;
-        this.reviews = reviews;
     }
 
+    public static Member toMemberEntity(MemberDTO memberDTO) {
+        Member member = new Member();
+        member.setKakao_id(memberDTO.getKakao_id());
+        member.setProfile_nickname(memberDTO.getProfile_nickname());
+        member.setProfile_image(memberDTO.getProfile_image());
+        member.setGender(memberDTO.getGender());
+        member.setAge(memberDTO.getAge());
+        return member;
+    }
 }
