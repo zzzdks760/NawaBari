e2328308f1b6cee771f8dfd0ae21c9805f499d5d
zzzdks760 <zzzdks760@naver.com>
2023-04-11 16:51:11 +0900

    toMemberDTO 생성

--- src/main/java/com/backend/NawaBari/dto/MemberDTO.java
+++ src/main/java/com/backend/NawaBari/dto/MemberDTO.java
@@ -1,8 +1,11 @@
 package com.backend.NawaBari.dto;
 
 import com.backend.NawaBari.domain.Member;
+import com.backend.NawaBari.domain.MemberZone;
+import com.backend.NawaBari.domain.review.Review;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,6 +13,7 @@ import java.util.stream.Collectors;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class MemberDTO {
     private Long id;
     private String kakao_id;
@@ -17,5 +21,19 @@ public class MemberDTO {
     private String profile_image;
     private String gender;
     private String age;
+    private List<MemberZone> memberZones = new ArrayList<>();
+    private List<Review> reviews = new ArrayList<>();
 
+
+    public static MemberDTO toMemberDTO(Member findMember) {
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(findMember.getId());
+        memberDTO.setKakao_id(findMember.getKakao_id());
+        memberDTO.setProfile_nickname(findMember.getProfile_nickname());
+        memberDTO.setProfile_image(findMember.getProfile_image());
+        memberDTO.setGender(findMember.getGender());
+        memberDTO.setAge(findMember.getAge());
+
+        return memberDTO;
+    }
 }
