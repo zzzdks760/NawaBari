8a1d6ab6fd0973a31893f250d5c9211d54f6d9f0
zzzdks760 <zzzdks760@naver.com>
2023-04-17 20:39:50 +0900

    카카오맵api 키워드검색으로 식당정보 불러오는 서비스 생성

--- src/main/java/com/backend/NawaBari/service/RestaurantLoadService.java
+++ src/main/java/com/backend/NawaBari/service/RestaurantLoadService.java
@@ -1,13 +1,22 @@
 package com.backend.NawaBari.service;
 
+import com.backend.NawaBari.domain.Base;
 import com.backend.NawaBari.domain.Restaurant;
-import com.backend.NawaBari.json.JsonFileLoader;
-import com.backend.NawaBari.repository.RestaurantRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
 @Service
@@ -15,13 +24,41 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RestaurantLoadService {
 
-    private final JsonFileLoader jsonFileLoader;
-    private final RestaurantRepository restaurantRepository;
 
-    public void SaveData() throws IOException {
 
-        List<Restaurant> zoneList = jsonFileLoader.loadRestaurantJsonData();
-        restaurantRepository.saveAll(zoneList);
+    private final String KAKAO_API_KEY = "27eb3015ee913717948781cb023560f3";
+    private final int PAGE_SIZE = 15;
+
+    public List<Restaurant> searchRestaurants(String query, int page) throws IOException {
+        String url = "https://dapi.kakao.com/v2/local/search/keyword.json";
+        url += "?query=" + URLEncoder.encode(query, "UTF-8");
+        url += "&page=" + page;
+        url += "&size=" + PAGE_SIZE;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Authorization", "KakaoAK " + KAKAO_API_KEY);
+        con.setRequestProperty("Content-Type", "application/json");
+
+        int responseCode = con.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<Restaurant> restaurantList = objectMapper.readValue(response.toString(), new TypeReference<List<Restaurant>>() {});
+            return restaurantList;
+        } else {
+            throw new RuntimeException("Failed to retrieve restaurants from Kakao API: " + con.getResponseMessage());
+        }
     }
 
 }
