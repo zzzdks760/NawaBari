323cf8712b62b38dd21c44cbb8923106a983624c
zzzdks760 <zzzdks760@naver.com>
2023-04-17 20:40:03 +0900

    레스토랑 dto생성

---
+++ src/main/java/com/backend/NawaBari/dto/RestaurantDTO.java
@@ -0,0 +1,35 @@
+package com.backend.NawaBari.dto;
+
+import com.backend.NawaBari.domain.Restaurant;
+import lombok.*;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestaurantDTO {
+
+    private String name;
+
+    private String address;
+
+    private Double lat;
+
+    private Double lng;
+
+    public static List<Restaurant> toEntityList(List<RestaurantDTO> restaurantDTOList) {
+        return restaurantDTOList.stream()
+                .map(restaurantDTO -> Restaurant.builder()
+                        .name(restaurantDTO.getName())
+                        .address(restaurantDTO.getAddress())
+                        .lat(restaurantDTO.getLat())
+                        .lng(restaurantDTO.getLng())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
