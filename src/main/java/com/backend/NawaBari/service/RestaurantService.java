b3664ddf9c26fa4123012c8748d6d1c4005f0140
zzzdks760 <zzzdks760@naver.com>
2023-04-18 16:41:25 +0900

    식당이름으로 조회하는 코드 생성

--- src/main/java/com/backend/NawaBari/service/RestaurantService.java
+++ src/main/java/com/backend/NawaBari/service/RestaurantService.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class RestaurantService {
 
@@ -17,16 +19,43 @@ public class RestaurantService {
 
 
     //식당 생성
+    @Transactional
     public Long createRestaurant(Restaurant restaurant) {
         restaurantRepository.save(restaurant);
         return restaurant.getId();
     }
 
     //식당 이름 조회
+    public List<Restaurant> findByRestaurantName(String name) {
+        if (name.length() < 2) {
+            throw new IllegalArgumentException("최소 두 글자 이상 필요합니다.");
+        }
+        List<Restaurant> restaurants = restaurantRepository.findByName(name);
+        if (restaurants.size() == 0) { //일치하는 식당이 없는 경우
+            //입력된 이름에서 최소 두 글자 이상이 연속된 문자가 같은 경우를 찾습니다.
+            for (int i = 0; i < name.length() - 1; i++) {
+                String sub = name.substring(i, i + 2);
+                List<Restaurant> temp = restaurantRepository.findByName(sub);
+                if (temp.size() > 0) { //일치하는 식당이 있는 경우
+                    return temp;
+                }
+            }
+        }
+        return restaurants;
+    }
 
-    //행정구역 코드로 식당 조회
+
+    //주소로 식당 조회
+    public List<Restaurant> findByAddressName(String address_name) {
+        if (address_name.length() < 2) {
+            throw new IllegalArgumentException("최소 두 글자 이상 필요합니다.");
+        }
+        return restaurantRepository.findRestaurantByAddress(address_name);
+
+    }
 
     //식당 수정
+    @Transactional
     public void updateRestaurant(Long restaurantId, String name, String restaurant_img, LocalTime openingTime, LocalTime closingTime, String address_name, String tel) {
         Restaurant restaurant = restaurantRepository.findOne(restaurantId);
 
@@ -40,6 +69,7 @@ public class RestaurantService {
 
 
     //식당 삭제
+    @Transactional
     public void deleteRestaurant(Long restaurantId) {
         restaurantRepository.delete(restaurantId);
     }
