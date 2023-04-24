735852dd23feee88b7d30733bc56e31bb47e8b89
zzzdks760 <zzzdks760@naver.com>
2023-04-24 16:42:23 +0900

    Update MemberApiController.java

--- src/main/java/com/backend/NawaBari/api/MemberApiController.java
+++ src/main/java/com/backend/NawaBari/api/MemberApiController.java
@@ -1,20 +1,30 @@
 package com.backend.NawaBari.api;
 
+import com.backend.NawaBari.converter.TokenConverter;
+import com.backend.NawaBari.dto.TokenDTO;
 import com.backend.NawaBari.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberApiController {
 
     private final MemberService memberService;
+    private final TokenConverter tokenConverter;
+
+    /**
+     * 사용자 토큰 받기
+     */
+    @PostMapping("/access_token")
+    public void getToken(@RequestParam TokenDTO tokenDTO) {
+        HashMap<String, Object> userInfo = memberService.getUserInfo(tokenDTO.getAccess_token());
+
+    }
 
     /**
      * 카카오 로그인
