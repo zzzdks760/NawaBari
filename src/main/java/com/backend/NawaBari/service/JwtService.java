3fa268bae92e9ee91e71c33501f360bbdd30114e
zzzdks760 <zzzdks760@naver.com>
2023-04-28 16:31:26 +0900

    jwt로직관련 클래스

---
+++ src/main/java/com/backend/NawaBari/service/JwtService.java
@@ -0,0 +1,175 @@
+package com.backend.NawaBari.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.backend.NawaBari.domain.Member;
+import com.backend.NawaBari.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Optional;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Getter
+public class JwtService {
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    @Value("${jwt.access.expiration}")
+    private Long accessTokenExpirationPeriod;
+
+    @Value("${jwt.refresh.expiration}")
+    private Long refreshTokenExpirationPeriod;
+
+    @Value("${jwt.access.header}")
+    private String accessHeader;
+
+    @Value("${jwt.refresh.header}")
+    private String refreshHeader;
+
+    private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    private static final String KAKAOID_CLAIM = "kakao_id";
+    private static final String BEARER = "Bearer";
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * AccessToken 생성메서드
+     */
+    public String createAccessToken (String kakao_id) {
+        LocalDateTime now = LocalDateTime.now();
+        return JWT.create() // JWT 토큰을 생성하는 빌더 반환
+                .withSubject(ACCESS_TOKEN_SUBJECT) // JWT의 Subject 지정 -> AccessToken이므로 AccessToken
+                .withExpiresAt(Date.from(Instant.now().plusSeconds(accessTokenExpirationPeriod))) // 토큰 만료 시간 설정
+                .withClaim(KAKAOID_CLAIM, kakao_id)
+                .sign(Algorithm.HMAC512(secretKey)); //HMAC512 알고리즘 사용, application-jwt.yml에서 지정한 secret 키로 암호화
+    }
+
+    /**
+     * RefreshToken 생성
+     * RefreshToken은 Claim에 kakao_id를 넣지 않으므로 withClaim() X
+     */
+    public String createRefreshToken() {
+        LocalDateTime now = LocalDateTime.now();
+        return JWT.create()
+                .withSubject(REFRESH_TOKEN_SUBJECT)
+                .withExpiresAt(Date.from(Instant.now().plusSeconds(refreshTokenExpirationPeriod)))
+                .sign(Algorithm.HMAC512(secretKey));
+    }
+
+    /**
+     * AccessToken 헤더에 실어서 보내기
+     */
+    public void sendAccessToken(HttpServletResponse response, String accessToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.setHeader(accessHeader, accessToken);
+        log.info("재발급된 Access Token : {}", accessToken);
+    }
+
+    /**
+     * AccessToken + RefreshToken 헤더에 실어서 보내기
+     */
+    public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        setAccessTokenHeader(response, accessToken);
+        setRefreshTokenHeader(response, refreshToken);
+        log.info("Access Token, Refresh Token 헤더 설정 완료");
+    }
+
+    /**
+     * 헤더에서 RefreshToken 추출
+     * 토큰 형식 : Bearer XXX에서 Bearer를 제외하고 순수 토큰만 가져오기 위해서
+     * 헤더를 가져온 후 "Bearer"를 삭제(""로 replace)
+     */
+    public Optional<String> extractRefreshToken(HttpServletResponse request) {
+        return Optional.ofNullable(request.getHeader(refreshHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    }
+
+    /**
+     * 헤더에서 AccessToken 추출
+     * 토큰 형식 : Bearer XXX에서 Bearer를 제외하고 순수 토큰만 가져오기 위해서
+     * 헤더를 가져온 후 "Bearer"를 삭제(""로 replace)
+     */
+    public Optional<String> extractAccessToken(HttpServletResponse request) {
+        return Optional.ofNullable(request.getHeader(accessHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    }
+
+    /**
+     * AccessToken에서 kakao_id 추출
+     * 추출 전에 JWT.require()로 검증기 생성
+     * verify로 AccessToken 검증 후
+     * 유효하다면 getClaim()으로 이메일 추출
+     * 유효하지 않다면 빈 Optional 객체 반환
+     */
+    public Optional<String> extractNickname(String accessToken) {
+        try {
+            //토큰 유효성 검사하는데 사용할 알고리즘이 있는 JWT verifier builder 반환
+            return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
+                    .build()
+                    .verify(accessToken)
+                    .getClaim(KAKAOID_CLAIM)
+                    .asString());
+        } catch (Exception e) {
+            log.error("엑세스 토큰이 유효하지 않습니다.");
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * AccessToken 헤더 설정
+     */
+    private void setRefreshTokenHeader(HttpServletResponse response, String refreshToken) {
+        response.setHeader(refreshHeader, refreshToken);
+    }
+
+    /**
+     * RefreshToken 헤더 설정
+     */
+    private void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
+        response.setHeader(accessHeader, accessToken);
+    }
+
+    /**
+     * RefreshToken DB저장(업데이트)
+     */
+    public void updateRefreshToken(String kakao_id, String refreshToken) {
+        Optional<Member> memberOptional = memberRepository.findByKakao_Id(kakao_id);
+        if (memberOptional.isPresent()) {
+            Member member = memberOptional.get();
+            member.updateRefreshToken(refreshToken);
+        } else {
+            throw new RuntimeException("일치하는 회원이 없습니다.");
+        }
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            JWT.require(Algorithm.HMAC512(secretKey)).build().verify(token);
+            return true;
+        } catch (Exception e) {
+            log.error("유효하지 않은 토큰입니다. {}", e.getMessage());
+            return false;
+        }
+    }
+
+}
