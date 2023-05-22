c33343bb54a8bb300ba525f611a017537f53e086
zzzdks760 <zzzdks760@naver.com>
2023-05-22 16:30:09 +0900

    비교 메서드 변경

--- src/main/java/com/backend/NawaBari/jwt/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/backend/NawaBari/jwt/JwtAuthenticationProcessingFilter.java
@@ -31,7 +31,7 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getRequestURI().startsWith(NO_CHECK_URL)) {
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
             filterChain.doFilter(request, response); // "/login" 요청이 들어오면, 다음 필터 호출
             return; // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
         }
