b22934d5d01491a13a9900166ad32ed0454d0e06
zzzdks760 <zzzdks760@naver.com>
2023-05-31 16:22:52 +0900

    리뷰작성 제한 설정

--- src/main/java/com/backend/NawaBari/service/ReviewService.java
+++ src/main/java/com/backend/NawaBari/service/ReviewService.java
@@ -1,7 +1,9 @@
 package com.backend.NawaBari.service;
 
 import com.backend.NawaBari.domain.Member;
+import com.backend.NawaBari.domain.MemberZone;
 import com.backend.NawaBari.domain.Restaurant;
+import com.backend.NawaBari.domain.Zone;
 import com.backend.NawaBari.domain.review.Photo;
 import com.backend.NawaBari.domain.review.Review;
 import com.backend.NawaBari.repository.MemberRepository;
@@ -30,13 +32,38 @@ public class ReviewService {
         Member member = memberRepository.findOne(memberId);
         Restaurant restaurant = restaurantRepository.findOne(restaurantId);
 
-        Review review = Review.createReview(member, restaurant, photos, title, content, rate);
-        restaurant.addReview(review);
-        restaurant.updateAverageRating();
-        restaurantRepository.save(restaurant);
-        reviewRepository.save(review);
+        boolean isAddressMatching = false;
+        for (MemberZone memberZone : member.getMemberZones()) {
+            if (checkAddress(memberZone.getZone(), restaurant.getAddress_name())) {
+                isAddressMatching = true;
+                break;
+            }
+        }
 
-        return review.getId();
+
+        if (isAddressMatching) { //구 주소가 일치하는 경우 리뷰생성
+            Review review = Review.createReview(member, restaurant, photos, title, content, rate);
+            restaurant.addReview(review);
+            restaurant.updateAverageRating();
+            restaurantRepository.save(restaurant);
+            reviewRepository.save(review);
+
+            return review.getId();
+        } else {
+            throw new IllegalArgumentException("리뷰를 작성할 수 있는 구역이 아닙니다.");
+        }
+
+    }
+
+    //추출한 구 이름과 식당주소가 일치하는지 판별
+    private boolean checkAddress(Zone zone, String address_name) {
+        String extractedDistrict = extractDistrictFromAddress(zone);
+        return address_name.contains(extractedDistrict);
+    }
+
+    //구역의 구이름 추출
+    private String extractDistrictFromAddress(Zone zone) {
+       return zone.getGu();
     }
 
     /**
