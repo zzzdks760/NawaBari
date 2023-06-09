923a8bd44b12bd0b1c81a61f2861c254af713d87
zzzdks760 <zzzdks760@naver.com>
2023-06-09 13:47:41 +0900

    식당 데이터 Load클래스

---
+++ src/main/java/com/backend/NawaBari/json/RestaurantsLoadService.java
@@ -0,0 +1,33 @@
+package com.backend.NawaBari.json;
+
+import com.backend.NawaBari.domain.Restaurant;
+import com.backend.NawaBari.domain.Zone;
+import com.backend.NawaBari.dto.RestaurantDTO;
+import com.backend.NawaBari.dto.ZoneDTO;
+import com.backend.NawaBari.repository.RestaurantRepository;
+import com.backend.NawaBari.repository.ZoneRepository;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RestaurantsLoadService {
+    private final JsonFileLoader jsonFileLoader;
+    private final RestaurantRepository restaurantRepository;
+
+    @Transactional
+    public void SaveData() throws IOException {
+
+        List<Restaurant> restaurants = jsonFileLoader.restaurantLoadJsonData();
+        restaurantRepository.saveAll(restaurants);
+    }
+}
