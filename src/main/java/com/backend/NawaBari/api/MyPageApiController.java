584b2cef85eaf4004863c3a07f66abf6f4a23403
zzzdks760 <zzzdks760@naver.com>
2023-07-14 16:24:48 +0900

    마이페이지 조회 코드간략화

--- src/main/java/com/backend/NawaBari/api/MyPageApiController.java
+++ src/main/java/com/backend/NawaBari/api/MyPageApiController.java
@@ -3,36 +3,31 @@ package com.backend.NawaBari.api;
 import com.backend.NawaBari.domain.Member;
 import com.backend.NawaBari.domain.Role;
 import com.backend.NawaBari.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class MyPageApiController {
     private final MemberService memberService;
 
-    @PostMapping("/api/v1/MyPage")
-    public MyPageDTO MyPage(@RequestParam Long memberId) {
-
+    /**
+     * 마이페이지 조회
+     */
+    @GetMapping("/api/v1/MyPage/{memberId}")
+    public MyPageDTO MyPage(@PathVariable Long memberId) {
         Member profile = memberService.profile(memberId);
 
-        MyPageDTO myPageDTO = new MyPageDTO();
-        myPageDTO.setId(profile.getId());
-        myPageDTO.setProfile_nickname(profile.getProfile_nickname());
-        myPageDTO.setProfile_image(profile.getProfile_image());
-        myPageDTO.setRole(profile.getRole());
-
-        return myPageDTO;
+        return new MyPageDTO(profile.getProfile_nickname(), profile.getProfile_image(), profile.getRole());
     }
 
 
     @Data
+    @AllArgsConstructor
     static class MyPageDTO {
-        private Long id;
         private String profile_nickname;
         private String profile_image;
         private Role role;
