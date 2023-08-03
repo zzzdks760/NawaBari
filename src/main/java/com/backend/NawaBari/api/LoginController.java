82a94641df37859d5e974300a036c372d011e235
zzzdks760 <zzzdks760@naver.com>
2023-08-03 13:20:59 +0900

    재 로그인 api생성

--- src/main/java/com/backend/NawaBari/api/LoginController.java
+++ src/main/java/com/backend/NawaBari/api/LoginController.java
@@ -1,32 +1,27 @@
-/*
 package com.backend.NawaBari.api;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
+import com.backend.NawaBari.jwt.JwtAuthenticationProcessingFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
-
+@RequiredArgsConstructor
 @RestController
 public class LoginController {
 
-    private final OAuth2AuthorizedClientService authorizedClientService;
+    private final JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter;
 
-    public LoginController(OAuth2AuthorizedClientService authorizedClientService) {
-        this.authorizedClientService = authorizedClientService;
+    @PostMapping("/refreshToken")
+    public ResponseEntity<?> refreshToken(@RequestHeader("Authorization") String refreshTokenHeader, HttpServletResponse response) {
+        // 클라이언트가 보낸 refreshTokenHeader 값은 "Bearer " 뒤에 실제 refreshToken이 붙어 있으므로,
+        // "Bearer "를 제거하여 실제 refreshToken만 추출한다.
+        String refreshToken = refreshTokenHeader.replace("Bearer ", "");
+        System.out.println("Received Refresh Token: " + refreshToken);
+
+        // 추출한 refreshToken을 JwtAuthenticationProcessingFilter의 메서드로 전달하여 처리한다.
+        // 이때, response 객체를 직접 넘겨줘서 필터 내부에서 sendAccessAndRefreshToken() 메서드를 사용할 수 있도록 한다.
+        jwtAuthenticationProcessingFilter.checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
+
+        return ResponseEntity.ok().build();
     }
-
-    @GetMapping("/loginSuccess")
-    public RedirectView getLoginInfo(Principal principal) {
-        OAuth2AuthorizedClient authorizedClient = authorizedClientService
-                .loadAuthorizedClient("kakao", principal.getName());
-
-        String accessToken = authorizedClient.getAccessToken().getTokenValue();
-
-        String redirectUri = "myapp://callback";
-        String finalRedirectUri = String.format("%s?access-token=%s", redirectUri, accessToken);
-
-        return new RedirectView(finalRedirectUri);
-    }
-}*/
+}
