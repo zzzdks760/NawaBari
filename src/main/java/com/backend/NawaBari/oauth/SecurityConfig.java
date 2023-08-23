9c73a2069959fc11944f2d7f097ce3335f66213f
zzzdks760 <zzzdks760@naver.com>
2023-08-23 23:14:39 +0900

    엔드포인트로 인가코드 받아오는 코드 추가

--- src/main/java/com/backend/NawaBari/oauth/SecurityConfig.java
+++ src/main/java/com/backend/NawaBari/oauth/SecurityConfig.java
@@ -26,6 +26,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
     private final JwtService jwtService;
     private final MemberRepository memberRepository;
+    private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
@@ -48,15 +49,25 @@ public class SecurityConfig {
 
                 // 아이콘, css, js 관련
                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
-                .requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
-                .requestMatchers("/zone/save", "/restaurant/save", "/api/memberZone", "/refreshToken", "/api/login/success").permitAll() // 회원구역설정 접근허용
+                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
+                .requestMatchers("/zone/save", "/restaurant/save", "/api/v1/location/restaurants", "/refreshToken",
+                        "/api/login/success", "/oauth2/callback/authorization", "/kakao/logout").permitAll()
                 .anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
                 .and()
+
                 //== 소셜 로그인 설정 ==//
                 .oauth2Login()
-                .successHandler(oAuth2LoginSuccessHandler) // 동의하고 계속하기를 눌렀을 때 Handler 설정
-                .failureHandler(oAuth2LoginFailureHandler); // 소셜 로그인 실패 시 핸들러 설정
-
+                    /*.authorizationEndpoint()
+                        .baseUri("/login/oauth2/code/kakao") //GET 요청이 들어오면 카카로로그인 페이지 리다이렉트
+                        .and()*/
+                    .redirectionEndpoint()
+                        .baseUri("/oauth2/callback/authorization") //Get 요청
+                        .and()
+                    .userInfoEndpoint()
+                        .userService(customOAuth2UserService)
+                        .and()
+                    .successHandler(oAuth2LoginSuccessHandler) // 동의하고 계속하기를 눌렀을 때 Handler 설정
+                    .failureHandler(oAuth2LoginFailureHandler); // 소셜 로그인 실패 시 핸들러 설정
 
         // 원래 스프링 시큐리티 필터 순서가 LogoutFilter 이후에 로그인 필터 동작
         // 따라서, LogoutFilter 이후에 우리가 만든 필터 동작하도록 설정
@@ -69,7 +80,6 @@ public class SecurityConfig {
 
     @Bean
     public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
-        JwtAuthenticationProcessingFilter jwtAuthenticationFilter = new JwtAuthenticationProcessingFilter(jwtService, memberRepository);
-        return jwtAuthenticationFilter;
+        return new JwtAuthenticationProcessingFilter(jwtService, memberRepository);
     }
 }
