b8c0887fbf4c23b834d3bb4c59ab5e3adb616e24
zzzdks760 <zzzdks760@naver.com>
2023-08-31 16:23:01 +0900

    재 로그인 엔드포인트 LoginController에서 AuthController로 이동하고 예외처리 추가하고 반환값 추가

--- src/main/java/com/backend/NawaBari/oauth2/AuthController.java
+++ src/main/java/com/backend/NawaBari/oauth2/AuthController.java
@@ -1,13 +1,18 @@
 package com.backend.NawaBari.oauth2;
 
+import com.backend.NawaBari.jwt.JwtAuthenticationProcessingFilter;
+import com.backend.NawaBari.jwt.JwtBlacklistService;
 import com.backend.NawaBari.jwt.JwtService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final OAuthLoginService oAuthLoginService;
     private final JwtService jwtService;
+    private final JwtBlacklistService jwtBlacklistService;
+    private final JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter;
 
+    /**
+     * 인가코드 받아 로그인처리후
+     * 헤더: 액세스토큰, 리프레시토큰, 회원아이디 반환
+     * 바디: 액세스토큰 유효기간(초), grantType 반환
+     */
     @PostMapping("/api/auth/kakao")
     public ResponseEntity<AddInfoResponse> loginKakao(@RequestBody KakaoLoginParams params, HttpServletResponse response) {
         AuthTokens authTokens = oAuthLoginService.login(params);
@@ -29,6 +41,29 @@ public class AuthController {
         response.setHeader("member_id", member_id.toString());
 
         return ResponseEntity.ok(new AddInfoResponse("Bearer", jwtService.getAccessTokenExpirationPeriod()));
+    }
+
+    /**
+     * 리프레시토큰을 받아 재로그인
+     */
+    @PostMapping("/api/reLogin")
+    public ResponseEntity<?> reLogin(@RequestHeader("Authorization-refresh") String refreshTokenHeader, HttpServletResponse response) {
+        // 클라이언트가 보낸 refreshTokenHeader 값은 "Bearer " 뒤에 실제 refreshToken이 붙어 있으므로,
+        // "Bearer "를 제거하여 실제 refreshToken만 추출한다.
+        String refreshToken = refreshTokenHeader.replace("Bearer ", "");
+
+        if (jwtBlacklistService.isTokenBlacklist(refreshToken)) {
+            String errorMessage = "이미 사용된 토큰입니다.";
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorMessage); //403응답
+        } else if (!jwtService.isTokenValid(refreshToken)) {
+            String errorMessage = "유효하지 않은 토큰입니다";
+            jwtBlacklistService.blacklistToken(refreshToken);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorMessage); //403응답
+        } else {
+            jwtAuthenticationProcessingFilter.checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
+
+            return ResponseEntity.ok().body(new AddInfoResponse("Bearer", jwtService.getAccessTokenExpirationPeriod()));
+        }
     }
 
     @Data
