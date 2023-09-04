189e52df22a189e4f9e350058d01711c039e60cf
zzzdks760 <zzzdks760@naver.com>
2023-09-04 23:47:14 +0900

    앱으로이동 버튼 추가 리턴

--- src/main/java/com/backend/NawaBari/api/AuthController.java
+++ src/main/java/com/backend/NawaBari/api/AuthController.java
@@ -46,7 +46,23 @@ public class AuthController {
        return  "redirect:webauthcallback://success?" +
                 "accessToken=" + accessToken +
                 "&refreshToken=" + refreshToken +
-                "&memberId=" + memberId;
+                "&memberId=" + memberId + "<!DOCTYPE html>" +
+                "<html lang='en'>" +
+                "<head>" +
+                "<meta charset='UTF-8'>" +
+                "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" +
+                "<title>Redirect to App Test</title>" +
+                "</head>" +
+                "<body>" +
+                "<button onclick='redirectToApp()'>앱으로 돌아가기</button>" +
+                "<script>" +
+                "function redirectToApp() {" +
+
+                "window.location.href = 'kakao18f952f0f1dbe4d4761234378f3bbf8d://oauth';" +
+                "}" +
+                "</script>" +
+                "</body>" +
+                "</html>";
     }
 
     /**
