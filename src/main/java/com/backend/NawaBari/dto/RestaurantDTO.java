5965e97bf898bb4816879210356b0c7065cdebdd
zzzdks760 <zzzdks760@naver.com>
2023-10-30 12:42:58 +0900

    식당 검색시 사용하는 DTO로 분리

--- src/main/java/com/backend/NawaBari/dto/RestaurantDTO.java
+++ src/main/java/com/backend/NawaBari/dto/RestaurantDTO.java
@@ -1,77 +1,36 @@
 package com.backend.NawaBari.dto;
 
-
-import com.backend.NawaBari.domain.Menu;
 import com.backend.NawaBari.domain.Restaurant;
-import lombok.*;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class RestaurantDTO {
     private Long restaurantId;
     private String name;
     private String main_photo_url;
     private int reviewCount;
-    private Double avgRating;
+    private double avgRating;
     private String topReviewTitle;
     private Long zoneId;
-
-/*    private String tel;
-    private String opening_hours;
-    private String holidays;
-
-    private String address_name;
-
-
-    private Map<String, String> menu;
-    private int bookMarkCount;
-    */
-
-    //식당데이터 삽입
-/*    public Restaurant toEntity() {
-        Restaurant restaurant = Restaurant.builder()
-                .name(this.name)
-                .address_name(this.address_name)
-                .tel(this.tel)
-                .opening_hours(this.opening_hours)
-                .holidays(this.holidays)
-                .main_photo_url(this.main_photo_url)
-                .build();
-
-        // Menu 엔터티 생성 및 매핑
-        List<Menu> menus = this.menu.entrySet().stream()
-                .map(entry -> {
-                    MenuDTO menuDTO = new MenuDTO(entry.getKey(), entry.getValue());
-                    return Menu.builder()
-                            .name(menuDTO.getName())
-                            .price(menuDTO.getPrice())
-                            .restaurant(restaurant)
-                            .build();
-                })
-                .collect(Collectors.toList());
-
-        restaurant.setMenus(menus); // Restaurant 엔터티에 메뉴 데이터 추가
-
-        return restaurant;
-    }*/
 
 
     public static RestaurantDTO convertToDTO(Restaurant restaurant) {
 
-            RestaurantDTO restaurantDTO = new RestaurantDTO();
+        RestaurantDTO restaurantDTO = new RestaurantDTO();
 
-            restaurantDTO.setRestaurantId(restaurant.getId());
-            restaurantDTO.setName(restaurant.getName());
-            restaurantDTO.setMain_photo_url(restaurant.getMain_photo_url());
-            restaurantDTO.setReviewCount(restaurant.getReviewCount());
-            restaurantDTO.setAvgRating(restaurant.getAvgRating());
-            restaurantDTO.setZoneId(restaurant.getZone().getId());
+        restaurantDTO.setRestaurantId(restaurant.getId());
+        restaurantDTO.setName(restaurant.getName());
+        restaurantDTO.setMain_photo_url(restaurant.getMain_photo_url());
+        restaurantDTO.setAvgRating(restaurant.getAvgRating());
+        restaurantDTO.setReviewCount(restaurant.getReviewCount());
+        restaurantDTO.setZoneId(restaurant.getZone().getId());
 
-            return restaurantDTO;
+        return restaurantDTO;
     }
 }
